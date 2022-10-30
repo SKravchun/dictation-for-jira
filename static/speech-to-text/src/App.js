3139b8f480f32614de61d70fb9f90cc109d312e9
Oleksandr Kravchun <oleksandr.krav@sassjet.com>
2022-10-30 16:12:26 +0200

    add text where the cursor is

--- static/speech-to-text/src/App.js
+++ static/speech-to-text/src/App.js
@@ -16,10 +16,10 @@ import Modal, {
 } from '@atlaskit/modal-dialog';
 import QuestionCircleIcon from '@atlaskit/icon/glyph/question-circle'
 import SuccessIcon from '@atlaskit/icon/glyph/check-circle';
-import { G300 } from '@atlaskit/theme/colors';
-import { gridSize } from '@atlaskit/theme/constants';
-import { token } from '@atlaskit/tokens';
-import { AutoDismissFlag, FlagGroup } from '@atlaskit/flag';
+import {G300} from '@atlaskit/theme/colors';
+import {gridSize} from '@atlaskit/theme/constants';
+import {token} from '@atlaskit/tokens';
+import {AutoDismissFlag, FlagGroup} from '@atlaskit/flag';
 
 
 function App() {
@@ -39,9 +39,18 @@ function App() {
     listening,
   } = useSpeechRecognition()
 
+  const textareaInput = useRef(null)
 
 
-  const addFlag = () => {
+  const descriptionFlag = () => {
+    const newFlagId = flags.length + 1;
+    const newFlags = flags.slice();
+    newFlags.splice(0, 0, newFlagId);
+
+    setFlags(newFlags);
+  };
+
+  const commentFlag = () => {
     const newFlagId = flags.length + 1;
     const newFlags = flags.slice();
     newFlags.splice(0, 0, newFlagId);
@@ -54,28 +63,28 @@ function App() {
   };
 
   const select = () => {
-    // const selection =
     return window.getSelection().toString()
+  }
+
+  function changeSelectedText(text) {
+    setArea(area.replace(select(), ` ${text} `))
   }
 
   useEffect(() => {
     if (select()) {
-      console.log(select())
       if (area.toLowerCase().includes(select().toLowerCase())) {
-        setArea(area.replace(select(), finalTranscript + ' '))
-        // setArea(text.replace(/ +/g, ' ').trim())
+        changeSelectedText(finalTranscript)
         resetTranscript()
       }
     } else if (finalTranscript) {
-      setArea(area + ' ' + firstSymbol(finalTranscript) + '.')
+      insertText(finalTranscript)
       resetTranscript()
     }
 
   }, [finalTranscript])
-  useEffect(() => {
 
+  useEffect(() => {
     view.getContext().then(data => setContext(data.extension.issue.key));
-    console.log(view.getContext())
   }, []);
 
 
@@ -102,23 +111,38 @@ function App() {
     return str[0].toUpperCase() + str.slice(1);
   }
 
-  const searchInput = useRef(null)
 
-  function handleFocus(){
-    searchInput.current.focus()
+  function handleFocus() {
+    textareaInput.current.focus()
+  }
+
+  // function cursorPosition(text){
+  //   let start = textareaInput.current.selectionStart
+  //   textareaInput.current.focus();
+  //   let caretPos = start + text.length + 1;
+  //   textareaInput.current.setSelectionRange(caretPos, caretPos);
+  // }
+
+  function insertText(text) {
+    let val = textareaInput.current.value;
+    let start = textareaInput.current.selectionStart, end = textareaInput.current.selectionEnd;
+    const inserted = ` ${firstSymbol(text)}. `
+    setArea(textareaInput.current.value = val?.slice(0, start) + inserted + val?.slice(end))
+    textareaInput.current.focus();
+    let caretPos = start + inserted.length;
+    textareaInput.current.setSelectionRange(caretPos, caretPos);
+
   }
 
   async function getIssue(issueKey) {
     const issueResponse = await requestJira(`/rest/api/3/issue/${issueKey}`);
     const response = await issueResponse.json()
-    return response.fields.description.content
+    return response?.fields?.description?.content
   }
 
   async function setDescription(text, issueKey) {
 
     const description = await getIssue(issueKey)
-    console.log(description)
-
     const fields = {}
 
     fields.description = {
@@ -129,21 +153,15 @@ function App() {
 
     if (text) {
       if (text && !description) {
-        fields.description = {
-          "type": "doc",
-          "version": 1,
+        fields?.description?.content.push({
+          "type": "paragraph",
           "content": [
             {
-              "type": "paragraph",
-              "content": [
-                {
-                  "text": text.trim(),
-                  "type": "text"
-                }
-              ]
+              "text": text.trim(),
+              "type": "text"
             }
           ]
-        }
+        })
       } else if (text && description) {
         description.forEach((arr) => {
           fields?.description?.content?.push(arr)
@@ -160,7 +178,7 @@ function App() {
       }
 
 
-      const issueResponse = await requestJira(`/rest/api/3/issue/${issueKey}`, {
+      const response = await requestJira(`/rest/api/3/issue/${issueKey}`, {
           method: 'PUT',
           headers: {
             'Accept': 'application/json',
@@ -169,8 +187,44 @@ function App() {
           body: JSON.stringify({fields})
         }
       );
+      descriptionFlag()
+
     }
-    addFlag()
+
+  }
+
+  async function createComment(text, issueKey) {
+
+    if (text) {
+      const fields = {
+        "body": {
+          "type": "doc",
+          "version": 1,
+          "content": [
+            {
+              "type": "paragraph",
+              "content": [
+                {
+                  "text": text.trim(),
+                  "type": "text"
+                }
+              ]
+            }
+          ]
+        }
+      }
+
+      const response = await requestJira(`/rest/api/3/issue/${issueKey}/comment`, {
+          method: 'POST',
+          headers: {
+            'Accept': 'application/json',
+            'Content-Type': 'application/json'
+          },
+          body: JSON.stringify(fields)
+        }
+      );
+      commentFlag()
+    }
 
   }
 
@@ -178,18 +232,21 @@ function App() {
     <Fragment>
       <div className="buttonWrapper">
         <div className="btnContainer">
-          <div
-            className="recordBtn"
-            style={{
-              backgroundColor: listening ? '#e41a1a' : '',
-              color: listening ? 'white' : '',
 
-            }}
-            onClick={() => {
-              handleListing()
-            }}>
-            <VidAudioOnIcon className="micro" label=""/>
-          </div>
+          <Tooltip content={listening ? 'Stop dictation' : 'Click to start dictate'}>
+            {(tooltipProps) => (
+              <div
+                {...tooltipProps}
+                className="recordBtn"
+                style={{
+                  backgroundColor: listening ? '#e41a1a' : '',
+                  color: listening ? 'white' : '',
+                }}
+                onClick={handleListing}>
+                <VidAudioOnIcon className="micro" label=""/>
+              </div>
+            )}
+          </Tooltip>
 
           <Button isDisabled={!area} onClick={openModal}>Clear</Button>
 
@@ -197,6 +254,14 @@ function App() {
             {(tooltipProps) => (
               <Button isDisabled={!area} {...tooltipProps} onClick={() => setDescription(area, context)}>
                 Append to Description
+              </Button>
+            )}
+          </Tooltip>
+
+          <Tooltip content="A comment will be created from the text below">
+            {(tooltipProps) => (
+              <Button isDisabled={!area} {...tooltipProps} onClick={() => createComment(area, context)}>
+                Create comment
               </Button>
             )}
           </Tooltip>
@@ -219,7 +284,7 @@ function App() {
         appearance="standard"
         placeholder="Enter your text here using keyboard or microphone"
         value={area}
-        ref={searchInput}
+        ref={textareaInput}
         onChange={(e) => setArea(e.target.value)}
       />
 
@@ -258,6 +323,26 @@ function App() {
               }
               key={flagId}
               title={`#${flagId} Description updated`}
+              description="Refresh the page to see the changes."
+            />
+          );
+        })}
+      </FlagGroup>
+
+      <FlagGroup onDismissed={handleDismiss}>
+        {flags.map((flagId) => {
+          return (
+            <AutoDismissFlag
+              id={flagId}
+              icon={
+                <SuccessIcon
+                  primaryColor={token('color.icon.success', G300)}
+                  label="Success"
+                  size="medium"
+                />
+              }
+              key={flagId}
+              title={`#${flagId} Comment created`}
               description="Refresh the page to see the changes."
             />
           );
